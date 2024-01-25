9a2a977289af8aed11bb81195960d3ee7be75a85
hidelgionovela <hidelgionovela@gmail.com>
2024-01-25 22:31:57 +0200

    feat(exception): add package for exceptions

---
+++ khoevent/src/main/java/com/bbm/khoevent/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bbm.khoevent.exception;
+
+public class EntityNotFoundException extends RuntimeException{
+
+
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+}
