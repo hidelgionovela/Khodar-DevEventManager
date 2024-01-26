1c8db86ab72ef8ec37664b5a37fed3b9abd70bd1
hidelgionovela <hidelgionovela@gmail.com>
2024-01-26 22:15:32 +0200

    Update TicketRepository.java

--- khoevent/src/main/java/com/bbm/khoevent/repository/TicketRepository.java
+++ khoevent/src/main/java/com/bbm/khoevent/repository/TicketRepository.java
@@ -1,7 +1,16 @@
 package com.bbm.khoevent.repository;
 
+import com.bbm.khoevent.model.Event;
 import com.bbm.khoevent.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    //Esse metódo vai fazer uma busca por todos os tickets pertencentes ao evento selecionado
+    List<Ticket> findAllByEventId(Long eventId);
+
+    //Esse metódo vai verificar se um usuário já está escrito no evento selecionado
+    boolean existsByAttendeeEmailAndEvent(String email, Event event);
 }
