15051887a60736aeacbb273fc15ed072f5b06f93
hidelgionovela <hidelgionovela@gmail.com>
2024-01-26 22:15:27 +0200

    Update ImageRepository.java

--- khoevent/src/main/java/com/bbm/khoevent/repository/ImageRepository.java
+++ khoevent/src/main/java/com/bbm/khoevent/repository/ImageRepository.java
@@ -3,5 +3,8 @@ package com.bbm.khoevent.repository;
 import com.bbm.khoevent.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByEventId(Long id);
 }
